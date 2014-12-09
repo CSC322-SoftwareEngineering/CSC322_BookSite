73d114a7937130f14156d6ceded291be09a0231c
dsrod92 <dsrod92@gmail.com>
2014-12-08 21:45:31 -0500

    fixed position of filter button

---
+++ EbookSystem/EbookSystem/Controllers/BrowseController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EbookSystem.Models;
+
+namespace EbookSystem.Controllers
+{
+    public class BrowseController : Controller
+    {
+
+        private SellDBContext db = new SellDBContext();
+        //
+        // GET: /Browse/
+
+        public ActionResult Index()
+        {
+            return View(db.Sell.ToList());
+        }
+
+        public ActionResult SearchIndex(string searchString)
+        {
+            var books  = from m in db.Sell
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(s => s.Title.Contains(searchString));
+            }
+
+            return View(books);
+        }
+
+    }
+}
